efd5af38ac9ace67b17a8ac46a8d5efb249bfa19
wass01 <maatallawassim01@gmail.com>
2021-03-23 16:19:56 +0100

    esercizio Terminato

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ var app = new Vue(
     data: {
       message: 'Hello',
       image: "https://clutchpoints.com/wp-content/uploads/2020/04/Zion-Williamson-.jpg",
+      dispN: "click",
+    },
+    methods: {
+      show: function() {
+        if(this.dispN == "click"){
+          this.dispN = "on";
+        } else if(this.dispN == "on"){
+          this.dispN = "click";
+        }
+      }
     }
   }
 );
